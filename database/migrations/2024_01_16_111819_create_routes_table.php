c9e45a6761d22d2d9546aea8692050d1c674cc59
Lou-Anne <saejinmx@gmail.com>
2024-01-16 12:33:28 +0100

    Début base de donnée

---
+++ database/migrations/2024_01_16_111819_create_routes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id('route_id');
+            $table->integer('route_short_name');
+            $table->string('route_long_name');
+            $table->string('route_color');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('routes');
+    }
+};
