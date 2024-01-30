c57cdac9e3c32ed67dbce2da1aeec3641d390885
Lou-Anne <saejinmx@gmail.com>
2024-01-30 16:28:11 +0100

    Création compte fonctionnel

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,11 +8,12 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    // /api/register
     public function register (Request $request){
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            ' password' => 'required|string|min:8',
+            'password' => 'required|string|min:8'
         ]);
 
         $user = User::create([
